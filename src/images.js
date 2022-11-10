0e5403345176ee6bae14d34a6b950b51f978c6ec
pinkipie11 <stani.simic22@gmail.com>
2022-11-10 05:20:54 +0100

    Aded readme

---
+++ src/images.js
@@ -0,0 +1,8 @@
+import image1 from './assets/house1.jpg';
+import image2 from './assets/house2.jpg';
+import image3 from './assets/house3.jpg';
+import image4 from './assets/house4.jpg';
+import image5 from './assets/house5.jpg';
+import image6 from './assets/house6.jpg';
+
+export default [image1, image2, image3, image4, image5, image6];
